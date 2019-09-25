b9580948ff30c1c61e7e71aad8ee579d794e4b03
FlorianMARTINEZ1 <florian.martinez01@etu.umontpellier.fr>
2019-09-25 21:33:43 +0200

    test bd

---
+++ php/Conf.php
@@ -0,0 +1,42 @@
+<?php
+class Conf
+{
+    private static $debug = true;
+    private static $databases = array(
+    // Le nom d'hote est webinfo a l'IUT
+    // ou localhost sur votre machine
+    'hostname' => 'webinfo',
+    // A l'IUT, vous avez une BDD nommee comme votre login
+    // Sur votre machine, vous devrez creer une BDD
+    'database' => 'martinezf',
+    // A l'IUT, c'est votre login
+    // Sur votre machine, vous avez surement un compte 'root'
+    'login' => 'martinezf',
+    // A l'IUT, c'est votre mdp (INE par defaut)
+    // Sur votre machine personelle, vous avez creez ce mdp a l'installation
+    'password' => 'projetFF8'
+  );
+
+    public static function getLogin()
+    {
+        //en PHP l'indice d'un tableau n'est pas forcement un chiffre.
+        return self::$databases['login'];
+    }
+
+    public static function getHostname()
+    {
+        return self::$databases['hostname'];
+    }
+    public static function getDatabase()
+    {
+        return self::$databases['database'];
+    }
+    public static function getPassword()
+    {
+        return self::$databases['password'];
+    }
+    public static function getDebug()
+    {
+        return self::$debug;
+    }
+}
