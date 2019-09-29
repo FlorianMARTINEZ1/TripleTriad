c26c359d72a9340e001f3193b398e3f238746849
FlorianMARTINEZ1 <florian.martinez01@etu.umontpellier.fr>
2019-09-29 14:55:35 +0200

    liste

--- script/scripts.js
+++ script/scripts.js
@@ -3,15 +3,16 @@ function allowDrop(ev) {
 }
 
 function drag(ev) {
-  ev.dataTransfer.setData("text", ev.target.id);
+  ev.dataTransfer.setData('text', ev.target.id);
 }
 
 function drop(ev) {
   ev.preventDefault();
-  var data = ev.dataTransfer.getData("text");
+  console.log(ev.target.classList[1]);
+  var data = ev.dataTransfer.getData('text');
   var img = ev.target.appendChild(document.getElementById(data));
-  ev.target.removeAttribute("ondrop");
-  img.setAttribute("pointer-events", "none");
-  img.removeAttribute("draggable");
-  img.removeAttribute("ondragstart");
+  ev.target.removeAttribute('ondrop');
+  img.setAttribute('pointer-events', 'none');
+  img.removeAttribute('draggable');
+  img.removeAttribute('ondragstart');
 }
