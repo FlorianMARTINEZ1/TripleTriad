8a6618e09f7c795bbadc239505f1dc02bf97c012
FlorianMARTINEZ1 <florian.martinez01@etu.umontpellier.fr>
2019-09-29 15:58:13 +0200

    class joueur avec recupération case jouée

--- script/scripts.js
+++ script/scripts.js
@@ -1,3 +1,27 @@
+class Joueur {
+  constructor(pseudo) {
+    this.pseudo = pseudo;
+    this.case = [];
+  }
+
+  ajouter(numcase) {
+    this.case.push(numcase);
+  }
+
+  afficher() {
+    for (const ncase of this.case) {
+      console.log(ncase);
+    }
+  }
+
+  toString() {
+    return `${this.pseudo}`;
+  }
+
+}
+
+var j1 = new Joueur('babou');
+
 function allowDrop(ev) {
   ev.preventDefault();
 }
@@ -8,11 +32,14 @@ function drag(ev) {
 
 function drop(ev) {
   ev.preventDefault();
-  console.log(ev.target.classList[1]);
   var data = ev.dataTransfer.getData('text');
   var img = ev.target.appendChild(document.getElementById(data));
   ev.target.removeAttribute('ondrop');
   img.setAttribute('pointer-events', 'none');
   img.removeAttribute('draggable');
   img.removeAttribute('ondragstart');
+  var j1 = new Joueur('babou');
+  j1.ajouter(ev.target.classList[1]);
+  console.log(j1.toString());
+  j1.afficher();
 }
