348097478adc374284de2e739207646797cf05c0
Sebastien-Gineste <sebastien.gineste@etu.umontpellier.fr>
2019-10-02 21:17:14 +0200

    add class card

---
+++ script/Card.js
@@ -0,0 +1,12 @@
+//Class Card
+
+class Card {
+  constructor(pseudo,valN,valS,valO,valE,Couleur) {
+    this.pseudo = pseudo;
+    this.valN=valN;
+    this.valS=valS;
+    this.valO=valO;
+    this.valE=valE;
+
+  }
+}
--- script/Game.js
+++ script/Game.js
@@ -1,9 +1,11 @@
 //Class GAME
 
 class Game {
-  constructor() {
+  constructor(id) {
     j1 = new Joueur("Joueur 1");
     j2 = new Joueur("Joueur 2");
     this.listJoueur = [j1, j2];
+    this.currentPlayer=0;
+    this.id = id;
   }
 }
