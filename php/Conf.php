f5e0de136ebc3eb9ec0e706c84065085a6e18b8f
FlorianMARTINNEZ1 <florian.martinez1@etu.umontpellier.fr>
2019-10-07 10:43:40 +0200

    model / conf

--- php/Conf.php
+++ php/Conf.php
@@ -5,7 +5,7 @@ class Conf
     private static $databases = array(
     // Le nom d'hote est webinfo a l'IUT
     // ou localhost sur votre machine
-    'hostname' => 'localhost',
+    'hostname' => 'webinfo',
     // A l'IUT, vous avez une BDD nommee comme votre login
     // Sur votre machine, vous devrez creer une BDD
     'database' => 'martinezf',
@@ -14,7 +14,7 @@ class Conf
     'login' => 'projet',
     // A l'IUT, c'est votre mdp (INE par defaut)
     // Sur votre machine personelle, vous avez creez ce mdp a l'installation
-    'password' => 'projet'
+    'password' => 'projetFF8'
   );
 
     public static function getLogin()
