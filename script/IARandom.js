68227419665a9365c0a3cda964c63bde8b9412c8
Grochocrum <pierre.maruejol@etu.umontpellier.fr>
2019-10-17 20:52:43 +0200

    Need testing but it might work (IA random)

--- script/IARandom.js
+++ script/IARandom.js
@@ -18,17 +18,20 @@ class IARandom extends Joueur {
     for (i = 0; i < 9; i++) {
       let N = document.getElementsByClassName('case' + i)[0].firstElementChild;
       if (N === null) {
-        casesVides.push(N)
+        casesVides.push(i)
       }
     }
 
   }
   play() {
-    setCasesVides();
-    x = getRandomIntInclusive(0, casesVides.length - 1);
-    y = getRandomIntInclusive(0, cards.length - 1);
-
-
+    setCasesVides(); //Regarder les cases vides et les récuperer dans la variable casesVides
+    var x = getRandomIntInclusive(0, casesVides.length - 1); //aléatoire pour sélectionner une case
+    var y = getRandomIntInclusive(0, cards.length - 1); //aléatoire pour sélectionner une carte
+    var cardPlayed = document.getElementById('drag' + cards[y].donneID()); // récupère la carte.
+    var img = document.getElementById('case' + casesVides[x]).appendChild(cardPlayed); //on met la carte dans la case.
+    confrontation(img.className, cardPlayed);
+    cards.remove(cardPlayed);
+    g2.setTurn();
   }
 
 
