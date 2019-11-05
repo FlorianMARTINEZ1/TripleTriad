1049a85c2287df85b607d31fac388805641c38b4
CemSARISOY <cem.sarisoy@etu.umontpellier.fr>
2019-11-05 17:44:56 +0100

    id cartes bd

--- script/ChercheCarte.js
+++ script/ChercheCarte.js
@@ -36,25 +36,25 @@ function readData(sData) {
     for (var i = 0; i < 10; i++) {
       tabId.push(getRandomIntInclusive(0, 111)); // choisie une carte entre 0 et 111
     }
-    var carte1 = new Card(1,cartes[tabId[0]]['nomCarte'], cartes[tabId[0]]['valN'], cartes[tabId[0]]['valS'],
+    var carte1 = new Card(parseInt(cartes[tabId[0]]['id']),cartes[tabId[0]]['nomCarte'], cartes[tabId[0]]['valN'], cartes[tabId[0]]['valS'],
       cartes[tabId[0]]['valO'], cartes[tabId[0]]['valE'], "bleue"); //  => créer une carte
-    var carte2 = new Card(2,cartes[tabId[1]]['nomCarte'], cartes[tabId[1]]['valN'], cartes[tabId[1]]['valS'],
+    var carte2 = new Card(parseInt(cartes[tabId[1]]['id']),cartes[tabId[1]]['nomCarte'], cartes[tabId[1]]['valN'], cartes[tabId[1]]['valS'],
       cartes[tabId[1]]['valO'], cartes[tabId[1]]['valE'], "bleue"); //  => créer une carte
-    var carte3 = new Card(3,cartes[tabId[2]]['nomCarte'], cartes[tabId[2]]['valN'], cartes[tabId[2]]['valS'],
+    var carte3 = new Card(parseInt(cartes[tabId[2]]['id']),cartes[tabId[2]]['nomCarte'], cartes[tabId[2]]['valN'], cartes[tabId[2]]['valS'],
       cartes[tabId[2]]['valO'], cartes[tabId[2]]['valE'], "bleue"); //  => créer une carte
-    var carte4 = new Card(4,cartes[tabId[3]]['nomCarte'], cartes[tabId[3]]['valN'], cartes[tabId[3]]['valS'],
+    var carte4 = new Card(parseInt(cartes[tabId[3]]['id']),cartes[tabId[3]]['nomCarte'], cartes[tabId[3]]['valN'], cartes[tabId[3]]['valS'],
       cartes[tabId[3]]['valO'], cartes[tabId[3]]['valE'], "bleue"); //  => créer une carte
-    var carte5 = new Card(5,cartes[tabId[4]]['nomCarte'], cartes[tabId[4]]['valN'], cartes[tabId[4]]['valS'],
+    var carte5 = new Card(parseInt(cartes[tabId[4]]['id']),cartes[tabId[4]]['nomCarte'], cartes[tabId[4]]['valN'], cartes[tabId[4]]['valS'],
       cartes[tabId[4]]['valO'], cartes[tabId[4]]['valE'], "bleue"); //  => créer une carte
-    var carte6 = new Card(6,cartes[tabId[5]]['nomCarte'], cartes[tabId[5]]['valN'], cartes[tabId[5]]['valS'],
+    var carte6 = new Card(parseInt(cartes[tabId[5]]['id']),cartes[tabId[5]]['nomCarte'], cartes[tabId[5]]['valN'], cartes[tabId[5]]['valS'],
       cartes[tabId[5]]['valO'], cartes[tabId[5]]['valE'], "rouge"); //  => créer une carte
-    var carte7 = new Card(7,cartes[tabId[6]]['nomCarte'], cartes[tabId[6]]['valN'], cartes[tabId[6]]['valS'],
+    var carte7 = new Card(parseInt(cartes[tabId[6]]['id']),cartes[tabId[6]]['nomCarte'], cartes[tabId[6]]['valN'], cartes[tabId[6]]['valS'],
       cartes[tabId[6]]['valO'], cartes[tabId[6]]['valE'], "rouge"); //  => créer une carte
-    var carte8 = new Card(8,cartes[tabId[7]]['nomCarte'], cartes[tabId[7]]['valN'], cartes[tabId[7]]['valS'],
+    var carte8 = new Card(parseInt(cartes[tabId[7]]['id']),cartes[tabId[7]]['nomCarte'], cartes[tabId[7]]['valN'], cartes[tabId[7]]['valS'],
       cartes[tabId[7]]['valO'], cartes[tabId[7]]['valE'], "rouge"); //  => créer une carte
-    var carte9 = new Card(9,cartes[tabId[8]]['nomCarte'], cartes[tabId[8]]['valN'], cartes[tabId[8]]['valS'],
+    var carte9 = new Card(parseInt(cartes[tabId[8]]['id']),cartes[tabId[8]]['nomCarte'], cartes[tabId[8]]['valN'], cartes[tabId[8]]['valS'],
       cartes[tabId[8]]['valO'], cartes[tabId[8]]['valE'], "rouge"); //  => créer une carte
-    var carte10 = new Card(10,cartes[tabId[9]]['nomCarte'], cartes[tabId[9]]['valN'], cartes[tabId[9]]['valS'],
+    var carte10 = new Card(parseInt(cartes[tabId[9]]['id']),cartes[tabId[9]]['nomCarte'], cartes[tabId[9]]['valN'], cartes[tabId[9]]['valS'],
       cartes[tabId[9]]['valO'], cartes[tabId[9]]['valE'], "rouge"); //  => créer une carte
 
     var tabCartes = [carte1, carte2, carte3, carte4, carte5, carte6, carte7, carte8, carte9, carte10];
