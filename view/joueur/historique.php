01892fd9f7e8aa4a02fbdbd82759e2b9216a1b62
CemSARISOY <cem.sarisoy@etu.umontpellier.fr>
2019-11-19 15:55:47 +0100

    cas où le joueur n'a jamais joué

--- view/joueur/historique.php
+++ view/joueur/historique.php
@@ -1,30 +1,36 @@
 <?php
 
     echo '<article> <div class="card-panel"><h3> Historique : </h3>';
+    if(!empty($tabHistorique)){
     foreach($tabHistorique as $value)
-    {
-        if($value['nomJ1'] == $_SESSION['login'])
-            {
-            if($value['scoreJ1']>$value['scoreJ2'])
-            {
-                echo '<div style="background-color:darkgreen;display: flex;justify-content: center;align-content: center;flex-direction: column;">Vous avez joué contre : '.$value['nomJ2'].'</br> Vous avez gagné '.$value['scoreJ1'].' à '.$value['scoreJ2'].' </div>';
-            }
-            else
-            {
-                echo '<div style="background-color:darkred;display: flex;justify-content: center;align-content: center;flex-direction: column;">Vous avez joué contre : '.$value['nomJ2'].'</br> Vous avez perdu '.$value['scoreJ1'].' à '.$value['scoreJ2'].'  </div>';
-            }
-        }
-        else
         {
-            if($value['scoreJ1']<$value['scoreJ2'])
-            {
-                echo '<div style="background-color:darkgreen;">'.$value[0].' </div>';
+            if($value['nomJ1'] == $_SESSION['login'])
+                {
+                if($value['scoreJ1']>$value['scoreJ2'])
+                {
+                    echo '<div style="background-color:darkgreen;display: flex;justify-content: center;align-content: center;flex-direction: column;">Vous avez joué contre : '.$value['nomJ2'].'</br> Vous avez gagné '.$value['scoreJ1'].' à '.$value['scoreJ2'].' </div>';
+                }
+                else
+                {
+                    echo '<div style="background-color:darkred;display: flex;justify-content: center;align-content: center;flex-direction: column;">Vous avez joué contre : '.$value['nomJ2'].'</br> Vous avez perdu '.$value['scoreJ1'].' à '.$value['scoreJ2'].'  </div>';
+                }
             }
             else
             {
-                echo '<div style="background-color:darkred;">'.$value[0].' </div>';
+                if($value['scoreJ1']<$value['scoreJ2'])
+                {
+                    echo '<div style="background-color:darkgreen;">'.$value[0].' </div>';
+                }
+                else
+                {
+                    echo '<div style="background-color:darkred;">'.$value[0].' </div>';
+                }
             }
         }
+    }
+    else
+    {
+        echo 'Vous n\'avez aucune partie dans votre historique !';
     }
 
 
