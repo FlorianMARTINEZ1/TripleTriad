9333eb81e47d309768129cb26fc2879884fcf2e8
Gineste-Sebastien <sebastien.gineste@etu.umontpellier.fr>
2019-11-22 12:52:01 +0100

    supprime game résoud bug

--- api/supprimeGame.php
+++ api/supprimeGame.php
@@ -5,11 +5,7 @@ require_once File::build_path(array('model','ModelGame.php'));
 
 $id= $_GET['id'];
 $game = ModelGame::select($id);
-<<<<<<< HEAD
-if($game->get(etat)=="attente"){ // si le 1er joueur à déja fini la partie, le 2éme la fini
-=======
 if($game->get("etat")=="attente"){ // si le 1er joueur à déja fini la partie, le 2éme la fini
->>>>>>> 2c31dbf7abebfa902c3ac5a1617e6cf06e58a5eb
   ModelGame::delete($id);
 }
 else{ // aucun joueur n'a encore terminer la partie
@@ -19,12 +15,6 @@ else{ // aucun joueur n'a encore terminer la partie
   );
   ModelGame::update($data);
 }
-<<<<<<< HEAD
-
-
-
-=======
->>>>>>> 2c31dbf7abebfa902c3ac5a1617e6cf06e58a5eb
 
 
 
