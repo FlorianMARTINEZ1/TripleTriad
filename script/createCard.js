9572522ab76c9b80d15992a6401933bed977e139
FlorianMARTINEZ1 <florian.martinez01@etu.umontpellier.fr>
2019-11-27 16:29:12 +0100

    carte

--- script/createCard.js
+++ script/createCard.js
@@ -1,4 +1,4 @@
-var loadFile = function(event) {
+/*var loadFile = function(event) {
   var output = document.getElementById('output');
   output.src = URL.createObjectURL(event.target.files[0]);
 
@@ -24,164 +24,126 @@ function makeBorder() {
 
 
 }
+console.log("Testing testing 1 2 3");
 Caman("#sample", "https://images.unsplash.com/photo-1500622944204-b135684e99fd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80", function() {
+  
   this.brightness(100000);
   this.contrast(20);
   this.render(function() {
     alert("Done!");
   });
 });
-/*
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+*/
+$(function() {
+  var canvasBlue = document.getElementById('canvasBlue');
+  var canvasRed = document.getElementById('canvasRed');
+  var ctx = canvasBlue.getContext('2d');
+  var cty = canvasRed.getContext('2d');
 
-let img = new Image();
-let fileName = '';
-
-const downloadBtn = document.getElementById('download-btn');
-const uploadFile = document.getElementById('upload-file');
-const revertBtn = document.getElementById('revert-btn');
-
-// Add Filters & Effects
-document.addEventListener('click', (e) => {
-  if (e.target.classList.contains('filter-btn')) {
-    if (e.target.classList.contains('brightness-add')) {
-      Caman('#canvas', img, function() {
-        this.brightness(5).render();
-      });
-    } else if (e.target.classList.contains('brightness-remove')) {
-      Caman('#canvas', img, function() {
-        this.brightness(-5).render();
-      });
-    } else if (e.target.classList.contains('contrast-add')) {
-      Caman('#canvas', img, function() {
-        this.contrast(5).render();
-      });
-    } else if (e.target.classList.contains('contrast-remove')) {
-      Caman('#canvas', img, function() {
-        this.contrast(-5).render();
-      });
-    } else if (e.target.classList.contains('saturation-add')) {
-      Caman('#canvas', img, function() {
-        this.saturation(5).render();
-      });
-    } else if (e.target.classList.contains('saturation-remove')) {
-      Caman('#canvas', img, function() {
-        this.saturation(-5).render();
-      });
-    } else if (e.target.classList.contains('vibrance-add')) {
-      Caman('#canvas', img, function() {
-        this.vibrance(5).render();
-      });
-    } else if (e.target.classList.contains('vintage-remove')) {
-      Caman('#canvas', img, function() {
-        this.vintage(-5).render();
-      });
-    } else if (e.target.classList.contains('vintage-add')) {
-      Caman('#canvas', img, function() {
-        this.vintage(5).render();
-      });
-    } else if (e.target.classList.contains('lomo-add')) {
-      Caman('#canvas', img, function() {
-        this.lomo().render();
-      });
-    } else if (e.target.classList.contains('clarity-add')) {
-      Caman('#canvas', img, function() {
-        this.clarity().render();
-      });
-    } else if (e.target.classList.contains('sincity-add')) {
-      Caman('#canvas', img, function() {
-        this.sinCity().render();
-      });
-    } else if (e.target.classList.contains('crossprocess-add')) {
-      Caman('#canvas', img, function() {
-        this.crossProcess().render();
-      });
-    } else if (e.target.classList.contains('pinhole-add')) {
-      Caman('#canvas', img, function() {
-        this.pinhole().render();
-      });
-    } else if (e.target.classList.contains('nostalgia-add')) {
-      Caman('#canvas', img, function() {
-        this.nostalgia().render();
-      });
-    } else if (e.target.classList.contains('hermajesty-add')) {
-      Caman('#canvas', img, function() {
-        this.herMajesty().render();
-      });
-    }
+  /* Enable Cross Origin Image Editing */
+  var img = new Image();
+  img.crossOrigin = '';
+  img.src = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/123941/koala.jpg';
+  var imgBordureD = new Image();
+  imgBordureD.crossOrigin = '';
+  imgBordureD.src = "../css/img/BordDore.png";
+  var imgBordureB = new Image();
+  imgBordureB.crossOrigin = '';
+  imgBordureB.src = "../css/img/BordBlanc.png";
+  img.onload = function() {
+    canvasBlue.width = img.width;
+    canvasBlue.height = img.height;
+    canvasRed.width = img.width;
+    canvasRed.height = img.height;
+    ctx.drawImage(img, 0, 0, img.width, img.height);
+    cty.drawImage(img,0,0,img.width,img.height);
   }
-});
 
-// Revert Filters
-revertBtn.addEventListener('click', (e) => {
-  Caman('#canvas', img, function() {
-    this.revert();
+  var $reset = $('#resetbtn');
+  var $red = $('#redbtn');
+  var $blue= $('#bluebtn');
+  var $resize = $('#resizebtn');
+  var $addBorder = $('#addBorderbtn')
+  var $save = $('#savebtn');
+
+
+  /* Creating custom filters */
+
+
+  $reset.on('click', function(e) {
+    $('input[type=range]').val(0);
+    Caman('#canvasBlue', img, function() {
+      this.revert(false);
+      this.render();
+    });
+  });
+
+  /* In built filters */
+  $red.on('click', function(e) {
+    Caman('#canvasRed', img, function() {
+      this.colorize(200, 0, 0, 50).render();
+    });
+  });
+
+  $blue.on('click',function(e) {
+    Caman('#canvasBlue', img, function() {
+      this.colorize(0, 0, 200, 50).render();
+    });
+  });
+  $resize.on('click',function(e){
+
+
+    Caman("#canvasBlue", function () {
+      this.resize({
+        width: 62,
+        height: 62
+      });
+      
+      // You still have to call render!
+      this.render();
+    });
+    Caman("#canvasRed", function () {
+      this.resize({
+        width: 62,
+        height: 62
+      });
+      // You still have to call render!
+      this.render();
+    });
+
+
+  })
+
+  $addBorder.on('click',function(e){
+    
+    Caman("#canvasBlue", function () {
+      this.newLayer(function () {
+        this.opacity(100);
+        this.setBlendingMode('screen');
+        this.overlayImage(imgBordureD);
+      });
+      
+      // You still have to call render!
+      this.render();
+    });
+    Caman("#canvasRed", function () {
+      this.newLayer(function () {
+        this.opacity(100);
+        this.setBlendingMode('screen');
+        this.overlayImage(imgBordureB);
+      });
+      // You still have to call render!
+      this.render();
+    });
+  })
+
+  /* You can also save it as a jpg image, extension need to be added later after saving image. */
+
+  $save.on('click', function(e) {
+    Caman('#canvasBlue', img, function() {
+      this.render(function() {
+        this.save('png');
+      });
+    });
   });
 });
-
-// Upload File
-uploadFile.addEventListener('change', (e) => {
-  // Get File
-  const file = document.getElementById('upload-file').files[0];
-
-  // Init FileReader
-  const reader = new FileReader();
-
-  if (file) {
-    // Set file name
-    fileName = file.name;
-    // Read data as URL
-    reader.readAsDataURL(file);
-  }
-
-  // Add image to canvas
-  reader.addEventListener('load', () => {
-    // Create img
-    img = new Image();
-    // Set src
-    img.src = reader.result;
-    // On image load, add to canvas
-    img.onload = function() {
-      canvas.width = img.width;
-      canvas.height = img.height;
-      ctx.drawImage(img, 0, 0, img.width, img.height);
-      canvas.removeAttribute('data-caman-id');
-    }
-  }, false);
-});
-
-// Download Event
-downloadBtn.addEventListener('click', (e) => {
-  // Get file ext
-  const fileExtension = fileName.slice(-4);
-
-  // Init new filename
-  let newFileName;
-
-  // Check image type
-  if (fileExtension === '.jpg' || fileExtension === '.png') {
-    newFileName = fileName.substring(0, fileName.length - 4) + '-edited.jpg';
-  }
-
-  // Call download
-  download(canvas, newFileName);
-});
-
-// Download function
-function download(canvas, fileName) {
-  // Init event
-  let e;
-  // Create link
-  const link = document.createElement('a');
-
-  // Set properties
-  link.download = filename;
-  link.href = canvas.toDataUrl('image/jpeg', 0.8);
-  // New mouse event
-  e = new MouseEvent('click');
-  // Dispatch event
-  link.dispatchEvent(e);
-}
-
-// My download button does not initiate any action*/
--- view/carte/createCard.php
+++ view/carte/createCard.php
@@ -7,7 +7,7 @@
 
     <body>
 
-    	<form method="post" action="#">
+   <!---<form method="post" action="#">
   <fieldset>
     <legend>Mon formulaire :</legend>
     <p>
@@ -49,11 +49,28 @@
     </p>
   </fieldset>
 
-</form>
+</form>-->
+<h1>Image Editor Demo Built with CamanJS</h1>
+<div class="col-lg-6">
+	<canvas id="canvasRed"></canvas>
+  	<canvas id="canvasBlue"></canvas>
+</div>
+<div class="col-lg-6">
+  <nav class="filters">
+    <button id="resetbtn" class="btn btn-success">Reset Photo</button>
+ 	<button id="savebtn" class="btn btn-success">Save Image</button>
+    <button id="redbtn" class="btn btn-primary">Red</button>
+    <button id="bluebtn" class="btn btn-primary">Blue</button>
+    <button id="resizebtn" class="btn btn-primary">Resize</button>
+    <button id="addBorderbtn" class="btn btn-primary">Add border</button>
+
+  </nav>
+</div>
 
     </body>
   <footer>
-    <script type="text/javascript" src="../../CamanJS-4.1.1/dist/caman.full.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/camanjs/4.0.0/caman.full.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script type="text/javascript" src="../../CamanJS-4.1.1/adapters/jquery.js"></script>
     <script type="text/javascript" src="../../script/createCard.js"></script>
   </footer>
