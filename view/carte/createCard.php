c0392971b6ae0ada8359eea877c9a8ab04c93434
FlorianMARTINEZ1 <florian.martinez01@etu.umontpellier.fr>
2019-12-17 10:56:09 +0100

    create card style

--- view/carte/createCard.php
+++ view/carte/createCard.php
@@ -1,37 +1,49 @@
-
-
-<div class="container">
-  <form class="CreateurCarte" method="post" action="./php/creerCarte.php">
-    <fieldset>
-      <legend>Mon formulaire :</legend>
-      <p>
-        <label for="nomCarte">Nom de la carte</label> :
-        <input type="text" placeholder="Ex : gilbert" name="nomCarte" id="nomCarte" required />
-      </p>
-      <p>
-        <label for="ValN">ValN</label> :
-        <input type="number" placeholder="Ex : 5" name="valN" id="ValN" min="1" max="10" oninput="onInput()" required />
-
-      </p>
-      <p>
-        <label for="ValS">ValS</label> :
-        <input type="number" placeholder="Ex : 5" name="valS" id="ValS" min="1" max="10" oninput="onInput()" required />
-
-      </p>
-      <p>
-        <label for="ValO">ValO</label> :
-        <input type="number" placeholder="Ex : 5" name="valO" id="ValO" min="1" max="10" oninput="onInput()" required />
-
-      </p>
-      <p>
-        <label for="ValE">ValE</label> :
-        <input type="number" placeholder="Ex : 5" name="valE" id="ValE" min="1" max="10" oninput="onInput()" required />
-
-      </p>
-      <div>
-        <label for="source">Source</label> :
-        <input type="text" value="autre" name="source" id="source"required />
-
+<div id="form-create" class="container">
+  <form id="create-card" class="CreateurCarte" method="post" action="./php/creerCarte.php">
+    <fieldset id="fieldcreate">
+      <legend>Créez votre carte</legend>
+        <div id="label-card" class="center">
+          <p>
+            <label for="nomCarte">Nom de la carte</label> 
+            <input type="text" placeholder="Ex : gilbert" name="nomCarte" id="nomCarte" maxlength="15" required />
+          </p>
+        </div>
+      <div class="row">
+        <div class="col s3">
+          <p>
+            <label for="ValN">ValN</label> 
+            <input type="number" placeholder="Ex : 5" name="valN" id="ValN" min="1" max="10" oninput="onInput()" required />
+          </p>
+        </div>
+        <div class="col s3">
+          <p>
+            <label for="ValS">ValS</label> 
+            <input type="number" placeholder="Ex : 5" name="valS" id="ValS" min="1" max="10" oninput="onInput()" required />
+          </p>
+        </div>
+        <div class="col s3">
+          <p>
+            <label for="ValO">ValO</label> 
+            <input type="number" placeholder="Ex : 5" name="valO" id="ValO" min="1" max="10" oninput="onInput()" required />
+          </p>
+        </div>
+        <div class="col s3">
+          <p>
+            <label for="ValE">ValE</label> 
+            <input type="number" placeholder="Ex : 5" name="valE" id="ValE" min="1" max="10" oninput="onInput()" required />
+          </p>
+        </div>
+      </div>
+      <div id="src">
+        <label for="source">Source</label> 
+        <div id="locaCard" class="center">
+          <select name="source" id="srcCard">
+            <option value="autre">Autre</option>
+            <option value="lol">League Of Legends</option>
+            <option value="smashbros">Super Smash Bros</option>
+            <option value="starcraft">Starcraft</option>
+          </select>
+        </div>
         <input type="file" accept="image/*" name="imageCanvas" onchange="readURL(this)" required>
         <div class="col-lg-6">
           <canvas id="canvasRed"></canvas>
