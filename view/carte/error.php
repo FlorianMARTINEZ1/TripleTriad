a43ea811a5bcad05a5d2edeec2941269d4199923
Sebastien-Gineste <sebastien.gineste@etu.umontpellier.fr>
2019-12-18 21:33:51 +0100

    supprimer fichier innutile

---
+++ view/carte/error.php
@@ -0,0 +1,13 @@
+
+
+
+        <?php
+
+        	echo '<article>
+
+
+          <div class="card-panel"><h3> ERROR 404 </h3>
+          <p> La carte n\'a pas été trouvé dans la base de donnée </p></div>
+          </article>';
+        ;
+        ?>
