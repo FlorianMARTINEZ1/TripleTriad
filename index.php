e146cd206ac80960c1596ba0e629048a8d92028f
Sebastien-Gineste <sebastien.gineste@etu.umontpellier.fr>
2019-12-18 22:04:55 +0100

    ajout de session_name()

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+session_name("TripleTriad_Q4_GINESTE-Sébastien_MARTINEZ-Florian_MARUEJOL-Pierre_SARISOY-Cem");
 session_start();
 require_once 'lib/File.php';
 require_once File::build_path(array('lib','Session.php'));
