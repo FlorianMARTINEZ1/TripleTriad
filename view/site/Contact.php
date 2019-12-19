4cee2f8d6b0ef94a7fabb6b2260c9fda98a1fcdd
FlorianMARTINNEZ1 <florian.martinez1@etu.umontpellier.fr>
2019-12-19 13:34:15 +0100

    presentation regle et contact

--- view/site/Contact.php
+++ view/site/Contact.php
@@ -1,1 +1,15 @@
 <!--Code pour la page contact ici --->
+<div class="container">
+    <div class="card">
+        <div class="card-content">
+            <form action="">
+
+                <div class="pseudo">input Pseudo</div>
+                <div class="subject">input sujet bouton select</div>
+                <div class="proove">input text-area ?</div>
+                <div class="send">input send</div>
+
+            </form>
+        </div>
+    </div>
+</div>
--- view/site/Regle.php
+++ view/site/Regle.php
@@ -1,1 +1,12 @@
 <!--Code pour la page règle ici --->
+<div class="container">
+
+    <div class="rules">
+        <div class="card">
+            <div class="card-content">
+                slide show
+            </div>
+        </div>
+    </div>
+
+</div>
