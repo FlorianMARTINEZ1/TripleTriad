68b96e6408b38e231bbecf973d3fe18411ca8175
Sebastien-Gineste <sebastien.gineste@etu.umontpellier.fr>
2019-12-28 15:00:57 +0100

    file attente

--- api/challenge.php
+++ api/challenge.php
@@ -2,11 +2,15 @@
 
 require_once '../lib/File.php';
 require_once File::build_path(array('model','ModelGame.php'));
+if(isset( $_POST['loginChallenged']) && isset($_POST['loginChallenger'])){
+  ModelGame::challenge($_POST['loginChallenged'],$_POST['loginChallenger']);
+  echo "Success";
+}
+else if(isset($_GET['loginChallenged']) && isset($_GET['loginChallenger'])){
+  ModelGame::challenge($_GET['loginChallenged'],$_GET['loginChallenger']);
+}
 
-$challenged = $_POST['loginChallenged'];
-$challenger = $_POST['loginChallenger'];
-ModelGame::challenge($challenged,$challenger);
-echo "Success";
+
 
 
 
