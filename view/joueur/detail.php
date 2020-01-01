a5648df8e524384f4899875585abd07a888131bb
CemSARISOY <cem.sarisoy@etu.umontpellier.fr>
2020-01-01 23:01:22 +0100

    fix division par zéro

--- view/joueur/detail.php
+++ view/joueur/detail.php
@@ -6,7 +6,18 @@
                 if(Session::is_user($j->get('login')) ||  Session::is_admin()){
                     echo '<p> Si vous voulez supprimer votre compte cliquez <a href="./index.php?action=delete&controller=joueur&login='.htmlspecialchars($j->get('login')).'" style="color: #0000b3;">ici</a> . </p>';
                     echo '<h4> Vos statistiques </h4>';
-                    echo '<div style="display:flex;width:50%;margin:auto"><div style="margin:auto"><h6>IA</h6><p>Nb parties : '.$nbPartie[0].'</p><p>Nb victoires : '.$nbWin[0].'</p><p>Ratio : '.intval(($nbWin[0]/$nbPartie[0])*100).'%</p></div><div style="margin:auto"><h6>Multi</h6><p>Nb parties : '.$nbPartie[1].'</p><p>Nb victoires : '.$nbWin[1].'</p><p>Ratio : '.intval(($nbWin[1]/$nbPartie[1])*100).'%</p></div><div style="margin:auto"><h6>Général</h6><p>Nb parties : '.intval($nbPartie[0]+$nbPartie[1]).'</p><p>Nb victoires : '.intval($nbWin[0]+$nbWin[1]).'</p><p>Ratio : '.intval((intval($nbWin[0]+$nbWin[1])/intval($nbPartie[0]+$nbPartie[1]))*100).'%</p></div></div>';
+                    echo '<div style="display:flex;width:50%;margin:auto"><div style="margin:auto"><h6>IA</h6>
+                    <p>Nb parties : '.$nbPartie[0].'</p><p>Nb victoires : '.$nbWin[0].'</p><p>Ratio : ';
+                    if($nbPartie[0]!=0){echo intval(($nbWin[0]/$nbPartie[0])*100).'%</p></div><div style="margin:auto">';}
+                    else{echo '0%</p></div><div style="margin:auto">';}
+                    echo'<h6>Multi</h6>
+                    <p>Nb parties : '.$nbPartie[1].'</p><p>Nb victoires : '.$nbWin[1].'</p><p>Ratio : ';
+                    if($nbPartie[1]!=0){echo intval(($nbWin[1]/$nbPartie[1])*100).'%</p></div><div style="margin:auto">';}
+                    else{echo '0%</p></div><div style="margin:auto">';}
+                    echo '<h6>Général</h6>
+                    <p>Nb parties : '.intval($nbPartie[0]+$nbPartie[1]).'</p><p>Nb victoires : '.intval($nbWin[0]+$nbWin[1]).'</p><p>Ratio : ';
+                    if(intval($nbPartie[0]+$nbPartie[1])!=0){ echo intval((intval($nbWin[0]+$nbWin[1])/intval($nbPartie[0]+$nbPartie[1]))*100).'%</p></div></div>';}
+                    else{echo '0%</p></div><div style="margin:auto">';}
                 }
           echo '</div>
             </arcticle>'
