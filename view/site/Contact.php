6e11404d2c04d712fe4d1888dc14df9d09258822
FlorianMARTINEZ1 <florian.martinez01@etu.umontpellier.fr>
2020-01-03 22:03:16 +0100

    Contact

--- view/site/Contact.php
+++ view/site/Contact.php
@@ -5,22 +5,30 @@
             <div class="card-content">
                <div class="container">
                 <form action="">
+                    <h4 class="center">Contact</h4>
                     <div class="row">
-                        <div class="pseudo input-field col s5">
+                        <div class="pseudo input-field col s6">
+                            <i class="material-icons prefix">account_circle</i>
                             <label id="label-contact" for="pseudo">Pseudo</label>
                             <input id="pseudo" type="text" name="pseudo" maxlength=20 require>
                         </div>
+                        <div class="email input-field col s6">
+                            <i class="material-icons prefix">email</i>
+                            <label id="label-contact" for="email">Email</label>
+                            <input id="email" type="email" name="email" require>
+                        </div>
                     </div>
                     <div class="subject input-field">
-                        <label id="label-contact" for="subject">Sujet</label>
+                        <i class="material-icons prefix">contact_support</i>
+                        <label id="label-contact" for="subject">Objet</label>
                         <input id="subject" type="text" name="subject"  maxlength=100 require>
                     </div>
                     <div class="proove input-field">
                         <label id="label-contact" for="detail-msg"></label>
-                        <textarea name="detail-msg" id="detail-msg" cols="30" rows="10" minlength=140 maxlength=1500></textarea>
+                        <textarea name="detail-msg" id="detail-msg" cols="30" rows="10" minlength=140 maxlength=1500 placeholder="Entrez votre message..."></textarea>
                     </div>
                     <div class="send">
-                        <input class="waves-effect waves-light ff8 btn " type="submit" value="Envoyer">
+                        <input class="waves-effect waves-light ff8 btn" type="submit" value="Envoyer">
                     </div>
 
                 </form>
